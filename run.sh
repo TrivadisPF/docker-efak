bfdb397af81cf72261a7d9d0ca697899211aa43b
gschmutz <guido.schmutz@trivadis.com>
2021-10-23 12:57:16 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sed -i "s/ZKSERVER/${ZKSERVER}/g" /app/kafka-eagle/conf/system-config.properties
+sed -i "s/ZKSERVER/${ZKSERVER}/g" /app/efak/conf/system-config.properties
 
-sh /app/kafka-eagle/bin/ke.sh start
+sh /app/efak/bin/ke.sh start
 sleep 3
-tail -f /app/kafka-eagle/logs/log.log
+tail -f /app/efak/logs/log.log
